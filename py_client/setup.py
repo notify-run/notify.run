54038b25d0f30107a04ccf71330ad818d339b153
Paul Butler <paulgb@gmail.com>
2018-08-04 12:41:49 -0400

    updated versions

--- py_client/setup.py
+++ py_client/setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='notify-run',
-      version='0.0.8',
+      version='0.0.9',
       python_requires='>=2.7',
       description='Client for notify.run notifications.',
       long_description=readme(),
