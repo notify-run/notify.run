55d39925964b2a9a1956fea7152b7efe4811f593
Paul Butler <paulgb@gmail.com>
2021-06-28 07:58:41 -0600

    version bump 0.14

--- py_client/setup.py
+++ py_client/setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='notify-run',
-      version='0.0.13',
+      version='0.0.14',
       python_requires='>=2.7',
       description='Client for notify.run notifications.',
       long_description=readme(),
